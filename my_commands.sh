49c75746eb08adc5c69ded7e01aad2016d48bff2
TejasMate <TejasMate@users.noreply.github.com>
2024-03-04 09:10:54 +0000

    Latest data: Mon Mar  4 09:10:54 UTC 2024

--- my_commands.sh
+++
@@ -1,3 +0,0 @@
-komac update --identifier iamscottxu.obs-rtspserver --version 3.1.0 --urls https://github.com/iamscottxu/obs-rtspserver/releases/download/v3.1.0/obs-rtspserver-v3.1.0-windows-installer.exe --submit --token
-komac update --identifier Terrastruct.D2 --version 0.6.3 --urls https://github.com/terrastruct/d2/releases/download/v0.6.3/d2-v0.6.3-windows-amd64.msi --submit --token
-komac update --identifier ThePyzoteam.pyzo --version 4.14.4 --urls https://github.com/pyzo/pyzo/releases/download/v4.14.4/pyzo-4.14.4-win64.exe --submit --token
