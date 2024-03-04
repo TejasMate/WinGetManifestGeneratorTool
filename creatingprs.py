f70ed751b1ce3f3f259bc4e9ffa12e11161f5e34
Tejas Mate <hi.tejas@pm.me>
2024-03-04 12:58:53 +0530

    Update creatingprs.py

--- creatingprs.py
+++ creatingprs.py
@@ -15,6 +15,14 @@ def check_string2(string):
     if re.fullmatch(r'[0-9.]+', string):
         return True
 
+def check_strings(value, string1, string2):
+    if string1 in value and string2 in value:
+        return True
+    else:
+        return False
+
+df_issues = pl.read_csv("OpenIssues.csv")
+
 pat = os.environ["TOKEN"]
 
 headers = {"Authorization": f"token {pat}",
@@ -24,74 +32,66 @@ headers = {"Authorization": f"token {pat}",
 df = pl.read_csv("GitHub_Releasess.csv")
 df_new = df.filter(pl.col('update_requires') == 'Yes')
 df_new = df_new.filter(pl.col('extension') != 'zip')
-df_new.write_csv("GitHub_Releasessss.csv")    
-
+df_new.write_csv("GitHub_Releasessss.csv")    
 commands = []
+
 for row in df_new.rows():
-    username, reponame, latest_ver, extension, pkgs_name, pkg_pattern, release_tag = row[0], row[1], row[2], row[3], row[4],  row[5], row[8]
-    if not release_tag: continue
+    username, reponame, extension, pkgs_name, pkg_pattern, release_tag = row[0], row[1], row[3], row[4],  row[5], row[8]  
 
-    download_urls = list()
-    # username = "AppFlowy-IO"
-    # reponame = "AppFlowy"
-    # release_tag = "0.5.0"  # Check the actual release tag from the Releases tab
-    
-    base_url = f"https://api.github.com/repos/{username}/{reponame}/releases/tags"
-    url = f"{base_url}/{release_tag}"
-    
-    if check_string1(release_tag.lower()):
-        pass
-    elif check_string1(release_tag.lower()):
-        pass
-    else:
-        continue
+    if not release_tag: continue
 
-    
-    
-    try:
-        response = requests.get(url, headers = headers)
-        response.raise_for_status()  # Raise an error for non-200 status codes
-    
-        if response.status_code == 200:
-            data = response.json()
-            if data:
-                for asset in data["assets"]:
-                    download_url = asset["browser_download_url"]
-                    download_urls.append(download_url)
-                    
-                    # print(f"Download URL: {download_url}")
-            else:
-                print(f"No releases found for {username}/{reponame} with tag {release_tag}")
-        else:
-            print(f"Error: {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        
+    download_urls = list()
+
+    base_url = f"https://api.github.com/repos/{username}/{reponame}/releases/tags"
+
+    url = f"{base_url}/{release_tag}"
+
+    if check_string1(release_tag.lower()):
+        pass
+    elif check_string1(release_tag.lower()):
+        pass
+    else:
+        continue
+
+    try:
+        response = requests.get(url, headers = headers)
+        response.raise_for_status()  # Raise an error for non-200 status codes    
+
+        if response.status_code == 200:
+            data = response.json()
+            if data:
+                for asset in data["assets"]:
+                    download_url = asset["browser_download_url"]
+                    download_urls.append(download_url)
+                    #print(f"Download URL: {download_url}")
+            else:
+                print(f"No releases found for {username}/{reponame} with tag {release_tag}")
+        else:
+            print(f"Error: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+    
     proper_urls = []
-    
-    for download_url in download_urls:
-        if f".{extension}" in download_url:
-            proper_urls.append(download_url) 
-        
-    print()
-    print(proper_urls)
 
-    
-    
-    if len(proper_urls) == 1:
-        komac_download_url = proper_urls[0]
-        komac_pkgs_name = pkgs_name
-        komac_version = release_tag.replace('v', '')
-        komac_version = komac_version.replace('V', '')
+    for download_url in download_urls:
+        if f".{extension}" in download_url:
+            proper_urls.append(download_url) 
 
-        if komac_version in latest_ver: continue
-        elif latest_ver in komac_version: continue
-        if latest_ver > komac_version: continue
-        
-        command = f"komac update --identifier {komac_pkgs_name} --version {komac_version} --urls {komac_download_url}"
-        commands.append(command)
-    
-print(commands)
+    komac_pkgs_name = pkgs_name
+    komac_version = release_tag.replace('v', '')
+    komac_version = komac_version.replace('V', '')
 
 
-    
+    for value in df_issues['Title']:
+        result = check_strings(value, komac_pkgs_name, komac_version)
+        if result: continue
+
+    if len(proper_urls) == 1:
+        komac_download_url = proper_urls[0]
+
+        command = f"komac update --identifier {komac_pkgs_name} --version {komac_version} --urls {komac_download_url} --submit --token"
+        commands.append(command)
+
+   
+with open("my_commands.sh", "w") as file:  # Open a file in write mode
+    file.write("\n".join(commands))  # Join elements with newlines and write to file
